c3a76907f06e847389bbffb7ac70e525604d40f2
rizkarhm <rizkarahma67@gmail.com>
2021-06-05 16:17:48 +0700

    update

--- admin/include/header.php
+++ admin/include/header.php
@@ -15,7 +15,7 @@
   <!-- Right navbar links -->
   <ul class="navbar-nav ml-auto">
     <li class="nav-item">
-      <a href="index.php?include=signout" class="nav-link">
+      <a onclick="if(confirm('Apakah anda yakin ingin Sign Out?')){ location.href='index.php?include=signout' }"  class="nav-link">
         Sign Out&nbsp;
         <i class="fas fa-sign-out-alt"></i>
       </a>
